14cf266e09dd9bdcf4de3656a9f8fb9dd83ab797
Salah Eldin Mohamed <salah.ahmed99@eng-st.cu.edu.eg>
2025-04-28 13:48:46 +0000

    Update-1

---
+++ 1. Data Structure Project/DataStructureProject/Classes/semlist.cs
@@ -0,0 +1,47 @@
+namespace DataStructureProject
+{
+    public class semlist<T>
+    {
+        #region Propertits
+        T[] Item;
+        public int Count {get;set:}
+        int CurrentIndex;
+        const int Capacity =4;
+        #endregion
+
+        #region Constructors
+        public semlist()
+        {
+            Item= new T[Capacity];
+            Count = 0;
+            CurrentIndex=0;
+        }
+        public semlist(int Capacity)
+        {
+            this.Capacity=Capacity;
+            this.Item= new T[Capacity];
+            Count = 0;
+            CurrentIndex=0;
+        }
+        #endregion
+        
+        #region Indexers
+        public T this[int index]
+        {
+            set
+            {
+                Item[index]=value
+            }
+            get
+            {
+                if(index>0 || index>Items.Length)
+                {
+                    throw new IndexOutOfRangeexception();
+                }
+                return Items[index]
+            }
+        }
+        public string this[string ListName]{}
+        #endregion
+    }
+}
