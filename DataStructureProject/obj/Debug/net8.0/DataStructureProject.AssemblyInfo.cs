5627bb9cca1844d6c2593134db2ddf33b1750a75
Salah Eldin Mohamed <salah.ahmed99@eng-st.cu.edu.eg>
2025-05-04 14:58:56 +0000

    updater

--- DataStructureProject/obj/Debug/net8.0/DataStructureProject.AssemblyInfo.cs
+++ DataStructureProject/obj/Debug/net8.0/DataStructureProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataStructureProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2830c0bacd4efb68126e500ad73647ec6d99fd6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f31b67db5a36172a6513ab403a3f250387fe76a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataStructureProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataStructureProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
